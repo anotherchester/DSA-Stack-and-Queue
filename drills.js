5cb05cf7fa87506501bd2f4dce5e5aff4632a8ba
anotherchester <anotherchester@gmail.com>
2020-04-23 21:59:07 -0700

    stack drills added

--- drills.js
+++ drills.js
@@ -6,4 +6,61 @@ starTrek.push('Spock');
 starTrek.push('McCoy');
 starTrek.push('Scotty');
 
-console.log(starTrek);
+console.log(starTrek)
+function peek(item) {
+  if (item.top === null) { return null }
+  else return item.top
+}
+
+console.log(peek(starTrek));
+
+function isEmpty(item) {
+  if (item.top) { return "Is not empty" }
+  else { return "Is empty" }
+}
+
+console.log(isEmpty(starTrek));
+
+
+function display(item) {
+  let current = item.top;
+  while (current) {
+    console.log(current.data);
+    current = current.next;
+  }
+  return current;
+}
+display(starTrek)
+
+function removeMcCoy(item) {
+  item.pop()
+  item.pop()
+  return item
+}
+console.log(removeMcCoy(starTrek))
+
+
+
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+
+
+
+
+  let stack = new Stack()
+  let strSplit = s.split('')
+  console.log(strSplit)
+  let emptyStr = ''
+  strSplit.forEach(char => stack.push(char))
+  for (let i = strSplit.length; i > 0; i--) {
+    emptyStr += stack.pop()
+  }
+  console.log(s === emptyStr)
+  return s === emptyStr
+}
+//True, true, true, false
+is_palindrome("dad");
+is_palindrome("A man, a plan, a canal: Panama");
+is_palindrome("1001");
+is_palindrome("Tauhida");
+
