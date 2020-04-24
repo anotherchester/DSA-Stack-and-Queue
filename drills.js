93e3d926c6e9821da11208b29a71e8067bbfeacd
anotherchester <anotherchester@gmail.com>
2020-04-24 09:57:19 -0700

    sort added

--- drills.js
+++ drills.js
@@ -64,3 +64,57 @@ is_palindrome("A man, a plan, a canal: Panama");
 is_palindrome("1001");
 is_palindrome("Tauhida");
 
+function matchingParentheses(expr) {
+  const stack = new Stack()
+  const match = {
+    '(': ')',
+    '[': '}',
+  }
+
+  for (let i = 0; i < expr.length; i++)
+  // if opening, push to stack
+  {
+    if (expr[i] === '(') { stack.push(expr[i]) }
+    //if closing, pop from stack
+    else if (expr[i] === ')') {
+      let last = stack.pop()
+      if (expr[i] !== match[last]) { return false }
+    }
+  }
+  if (stack.top !== null) { return false }
+  return true
+}
+console.log('Matching:', matchingParentheses('()()(())'))
+console.log(`Matching: `, matchingParentheses('(((()'))
+
+
+function createStack() {
+  const newStack = new Stack
+  newStack.push(10)
+  newStack.push(9)
+  newStack.push(6)
+  newStack.push(7)
+  newStack.push(8)
+
+  return newStack
+}
+
+function sortStack(item) {
+  // create a temporary stack
+  const tempStack = new Stack
+  // while input is not empty
+  while (item.top) {
+    // pop element from input
+    let temp = item.pop()
+    //while temp stack not empy and top of temp stack is less than temp
+    while (tempStack.top && tempStack.top.data < temp) {
+      // pop from temp stack and push to input stack
+      item.push(tempStack.pop())
+    }
+    //push temp to temp stack
+    tempStack.push(temp)
+  }
+  console.log(display(tempStack))
+}
+
+sortStack(createStack())
